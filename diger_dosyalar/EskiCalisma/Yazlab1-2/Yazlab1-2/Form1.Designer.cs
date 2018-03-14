af9df0616f0ae7a85b1e8d30198d84b8968bcbe8
mgrfn <eyildizpro@gmail.com>
2018-03-15 01:36:57 +0300

    eski calisma

---
+++ diger_dosyalar/EskiCalisma/Yazlab1-2/Yazlab1-2/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Yazlab1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_watchPath = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_replaceFileNames = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_originalFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(298, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "gözat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_watchPath
+            // 
+            this.textBox_watchPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox_watchPath.Location = new System.Drawing.Point(100, 21);
+            this.textBox_watchPath.Name = "textBox_watchPath";
+            this.textBox_watchPath.Size = new System.Drawing.Size(200, 23);
+            this.textBox_watchPath.TabIndex = 1;
+            this.textBox_watchPath.Text = "C:\\Users\\Radmin\\Downloads";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_replaceFileNames);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_originalFileName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox_watchPath);
+            this.groupBox1.Location = new System.Drawing.Point(9, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 204);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ayarlar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(6, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "R. D. Adları:";
+            // 
+            // textBox_replaceFileNames
+            // 
+            this.textBox_replaceFileNames.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox_replaceFileNames.Location = new System.Drawing.Point(100, 95);
+            this.textBox_replaceFileNames.Multiline = true;
+            this.textBox_replaceFileNames.Name = "textBox_replaceFileNames";
+            this.textBox_replaceFileNames.Size = new System.Drawing.Size(241, 93);
+            this.textBox_replaceFileNames.TabIndex = 5;
+            this.textBox_replaceFileNames.Text = "test.zip,test2.zip";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = " D. Adı:";
+            // 
+            // textBox_originalFileName
+            // 
+            this.textBox_originalFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox_originalFileName.Location = new System.Drawing.Point(100, 59);
+            this.textBox_originalFileName.Name = "textBox_originalFileName";
+            this.textBox_originalFileName.Size = new System.Drawing.Size(241, 23);
+            this.textBox_originalFileName.TabIndex = 3;
+            this.textBox_originalFileName.Text = "prolab.zip";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Klasör :";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 223);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Yazlab1-2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_watchPath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_replaceFileNames;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_originalFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ diger_dosyalar/EskiCalisma/Yazlab1-2/Yazlab1-2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yazlab1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox_watchPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        bool flag = false;
+
+        private void button_basla_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void Watch()
+        {
+            FileSystemWatcher fsw = new FileSystemWatcher();
+            fsw.Filter = "*.zip*";
+            fsw.IncludeSubdirectories = true;
+            fsw.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.FileName;
+            fsw.Path = textBox_watchPath.Text;
+            fsw.EnableRaisingEvents = true;
+
+            fsw.Renamed += new RenamedEventHandler(fsw_Renamed);
+
+            ChangeReplaceFile();
+        }
+
+        string replaceFileName = "";
+
+        void fsw_Renamed(object sender, RenamedEventArgs e)
+        {
+            if (!flag)
+            {
+                if (e.Name == textBox_originalFileName.Text)
+                {
+                    flag = true;
+
+                    if (replaceFileName != "")
+                    {
+                        string replaceFile = Path.Combine(textBox_watchPath.Text, replaceFileName);
+                        while (!File.Exists(replaceFile))
+                        {
+                            ChangeReplaceFile();
+                            replaceFile = Path.Combine(textBox_watchPath.Text, replaceFileName);
+                        }
+
+                        string fakeFileName = Path.Combine(textBox_watchPath.Text, "ornek_veri_dosyası_" + (new Random().Next(0, 100024) + replaceFileIndex).ToString() + ".xlsx");
+
+                        File.Move(e.FullPath, fakeFileName);
+
+                        if (File.Exists(e.FullPath))
+                            return;
+
+                        File.Move(replaceFile, e.FullPath);
+
+                        Application.Exit();
+
+                    }
+                }
+            }
+        }
+
+
+        int replaceFileIndex = 0;
+        public void ChangeReplaceFile()
+        {
+            string[] replaceFiles = textBox_replaceFileNames.Text.Split(',');
+
+            if (replaceFileIndex < replaceFiles.Length)
+            {
+                replaceFileName = replaceFiles[replaceFileIndex];
+                replaceFileIndex++;
+            }
+            else
+            {
+                replaceFileName = "";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //notifyIcon1.MouseDoubleClick += new MouseEventHandler(MyIcon_MouseDoubleClick);
+            notifyIcon1.Click += new EventHandler(MyIcon_MouseDoubleClick2);
+
+            timer1.Start();
+            
+            Watch();
+        }
+
+
+        void MyIcon_MouseDoubleClick2(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Hide();
+
+            timer1.Stop();
+        } 
+
+
+
+    }
+}
